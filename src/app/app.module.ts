bc4e378c60d748706a1cd67f533e8381052490fc
nicktabolich <nicktabolich@gmail.com>
2017-04-09 19:26:25 +0300

    2.1.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Router, NavigationStart, NavigationEnd, RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { routing } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent} from './components/top-bar/top-bar.component';
+import { MenuLeftComponent } from './components/menu-left/menu-left.component';
+import { MenuRightComponent } from './components/menu-right/menu-right.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { StructureModule } from './structure/structure.module';
+
+declare var NProgress: any;
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        TopBarComponent,
+        MenuLeftComponent,
+        MenuRightComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule,
+        StructureModule,
+        NgbModule.forRoot(),
+        routing
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
+})
+
+export class AppModule {
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+
+      if(event instanceof NavigationStart) {
+        NProgress.start();
+      }
+
+      if(event instanceof NavigationEnd) {
+        setTimeout(function(){
+          NProgress.done();
+        }, 200);
+      }
+
+    });
+  }
+}
---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cat-top-bar',
+  templateUrl: './top-bar.component.html',
+})
+export class TopBarComponent {}
