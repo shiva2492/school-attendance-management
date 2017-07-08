9f01920a2e9b55ce3554a77f02d18b90b37cfd64
shiva2492 <shivadwivediit@gmail.com>
2017-07-08 17:56:52 +0530

    changes in project title and routes comment removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component'; //import about component
+import { DashboardComponent } from './dashboard/dashboard.component'; 
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent } // redirect to home page on load
+  { path: 'dashboard', component: DashboardComponent } 
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
